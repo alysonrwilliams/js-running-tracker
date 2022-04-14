d1339102e932d059538e75bfe2040f6d58d623ce
Alyson Williams <alysonrwilliams@gmail.com>
2022-04-13 20:09:36 -0400

    submit event on form

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  const entry = Number(document.querySelector('#entry').value);
+  console.log(entry);
+}
+
+const form = document
+  .querySelector('form')
+  .addEventListener('submit', handleSubmit);
